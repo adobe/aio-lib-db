92febaadf119e4a61334b46e0f82ebe2d74545e8
Peter Dohogne <pdohogne@adobe.com>
2025-05-14 15:57:13 -0400

    CEXT-4509: Adding db.listCollections() and api call refactoring

---
+++ utils/apiRequest.js
@@ -0,0 +1,80 @@
+const { ENDPOINT_URL } = require('../lib/constants')
+const AbdbError = require('../lib/abdbError')
+const { EJSON } = require("bson")
+
+/**
+ * Hit the abdb proxy service API and return the data field from the result
+ *
+ * @param {AxiosStatic} axiosClient
+ * @param {string} apiPath abdb api url past <ENDPOINT>/v1/
+ * @param {string=} tenantId
+ * @param {Object=} params
+ * @param {Object=} options
+ * @returns {Promise<*>}
+ * @throws {AbdbError}
+ */
+async function apiPost(axiosClient, apiPath, tenantId = undefined, params = {}, options = {}) {
+  const endpoint = apiPath.match(/([^/]+)$/g)[0]
+  let res
+  try {
+    /** @type {Object|null}
+     * @mixes AxiosRequestConfig */
+    const tenantHeader = tenantId ? { headers: { 'x-abdb-tenant-id': tenantId } } : {}
+    const body = params
+    if (options) {
+      body.options = options
+    }
+    res = await axiosClient.post(
+      `${ENDPOINT_URL}/v1/${apiPath}`,
+      EJSON.stringify(body, { relaxed: false }),
+      tenantHeader
+    )
+  }
+  catch (err) {
+    if (err.response?.data) {
+      throw new AbdbError(`${endpoint} failed: ${err.response.data.message}`, { cause: err })
+    }
+    throw err
+  }
+
+  if (!res.data.success) {
+    throw new AbdbError(`${endpoint} failed: ${res.data.message}`)
+  }
+  return res.data.data
+}
+
+/**
+ * Hit the abdb proxy service API and return the data field from the result
+ *
+ * @param {AxiosStatic} axiosClient
+ * @param {string} apiPath abdb api url past <ENDPOINT>/v1/
+ * @param {string=} tenantId
+ * @returns {Promise<*>}
+ * @throws {AbdbError}
+ */
+async function apiGet(axiosClient, apiPath, tenantId = undefined) {
+  const endpoint = apiPath.match(/([^/]+)$/g)[0]
+  let res
+  try {
+    /** @type {Object|null}
+     * @mixes AxiosRequestConfig */
+    const tenantHeader = tenantId ? { headers: { 'x-abdb-tenant-id': tenantId } } : {}
+    res = await axiosClient.get(`${ENDPOINT_URL}/v1/${apiPath}`, tenantHeader)
+  }
+  catch (err) {
+    if (err.response?.data) {
+      throw new AbdbError(`${endpoint} failed: ${err.response.data.message}`, { cause: err })
+    }
+    throw err
+  }
+
+  if (!res.data.success) {
+    throw new AbdbError(`${endpoint} failed: ${res.data.message}`)
+  }
+  return res.data.data
+}
+
+module.exports = {
+  apiPost,
+  apiGet
+}
