832c4c9a8d990b2185634d3b2c356882dec49c0c
Peter Dohogne <pdohogne@adobe.com>
2025-05-28 15:36:22 -0400

    CEXT-4636: provisionRequest and provisionStatus

--- utils/apiRequest.js
+++ utils/apiRequest.js
@@ -1,41 +1,39 @@
-const { ENDPOINT_URL } = require('../lib/constants')
+const { ENDPOINT_URL, RUNTIME_HEADER, TENANT_HEADER } = require('../lib/constants')
 const AbdbError = require('../lib/abdbError')
 const { EJSON } = require("bson")
 
 /**
  * Hit the abdb proxy service API and return the data field from the result
  *
- * @param {AxiosStatic} axiosClient
+ * @param {Abdb} abdb
  * @param {string} apiPath abdb api url past <ENDPOINT>/v1/
- * @param {string=} tenantId
  * @param {Object=} params
  * @param {Object=} options
+ * @param {string=} tenantId TODO: Remove tenancy in favor of runtime, see CEXT-4671
  * @returns {Promise<*>}
  * @throws {AbdbError}
  */
-async function apiPost(axiosClient, apiPath, tenantId = undefined, params = {}, options = {}) {
+async function apiPost(abdb, apiPath, params = {}, options = {}, tenantId = undefined) {
   const endpoint = apiPath.substring(apiPath.lastIndexOf('/') + 1)
   let res
   try {
     /** @type {Object}
      * @mixes AxiosRequestConfig */
     const reqConfig = {
-      headers: {
-        'x-runtime-namespace': 'RUNTIME_PLACEHOLDER', // TODO: Implement runtime namespace validation, see CEXT-4617
-      },
+      headers: { [RUNTIME_HEADER]: abdb.runtimeNamespace },
       auth: { // TODO: Implement basic auth, see CEXT-4617
         username: 'user',
         password: 'pass'
       }
     }
-    if (tenantId) {
-      reqConfig.headers['x-abdb-tenant-id'] = tenantId
+    if (tenantId || abdb.tenantId) {
+      reqConfig.headers[TENANT_HEADER] = tenantId || abdb.tenantId
     }
     const body = params
     if (options) {
       body.options = options
     }
-    res = await axiosClient.post(
+    res = await abdb.axiosClient.post(
       `${ENDPOINT_URL}/v1/${apiPath}`,
       EJSON.stringify(body, { relaxed: false }),
       reqConfig
@@ -57,31 +55,29 @@ async function apiPost(axiosClient, apiPath, tenantId = undefined, params = {}, 
 /**
  * Hit the abdb proxy service API and return the data field from the result
  *
- * @param {AxiosStatic} axiosClient
+ * @param {Abdb} abdb
  * @param {string} apiPath abdb api url past <ENDPOINT>/v1/
- * @param {string=} tenantId
+ * @param {string=} tenantId TODO: Remove tenancy in favor of runtime, see CEXT-4671
  * @returns {Promise<*>}
  * @throws {AbdbError}
  */
-async function apiGet(axiosClient, apiPath, tenantId = undefined) {
+async function apiGet(abdb, apiPath, tenantId = undefined) {
   const endpoint = apiPath.substring(apiPath.lastIndexOf('/') + 1)
   let res
   try {
     /** @type {Object}
      * @mixes AxiosRequestConfig */
     const reqConfig = {
-      headers: {
-        'x-runtime-namespace': 'RUNTIME_PLACEHOLDER', // TODO: Implement runtime namespace validation, see CEXT-4617
-      },
+      headers: { [RUNTIME_HEADER]: abdb.runtimeNamespace },
       auth: { // TODO: Implement basic auth, see CEXT-4617
         username: 'user',
         password: 'pass'
       }
     }
-    if (tenantId) {
-      reqConfig.headers['x-abdb-tenant-id'] = tenantId
+    if (tenantId || abdb.tenantId) {
+      reqConfig.headers[TENANT_HEADER] = tenantId || abdb.tenantId
     }
-    res = await axiosClient.get(`${ENDPOINT_URL}/v1/${apiPath}`, reqConfig)
+    res = await abdb.axiosClient.get(`${ENDPOINT_URL}/v1/${apiPath}`, reqConfig)
   }
   catch (err) {
     if (err.response?.data) {
