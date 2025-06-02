0fc4cc0fe95381a47d5661c4364b64611c576280
Peter Dohogne <pdohogne@adobe.com>
2025-06-02 15:07:22 -0400

    CEXT-4702: Adding request id to errors

--- utils/apiRequest.js
+++ utils/apiRequest.js
@@ -1,9 +1,9 @@
-const { ENDPOINT_URL, RUNTIME_HEADER, TENANT_HEADER } = require('../lib/constants')
+const { ENDPOINT_URL, RUNTIME_HEADER, TENANT_HEADER, REQUEST_ID_HEADER } = require('../lib/constants')
 const AbdbError = require('../lib/abdbError')
 const { EJSON } = require("bson")
 
 /**
- * Hit the abdb proxy service API and return the data field from the result
+ * Execute a POST request to the abdb proxy service API and return the data field from the result
  *
  * @param {Abdb} abdb
  * @param {string} apiPath abdb api url past <ENDPOINT>/v1/
@@ -14,46 +14,15 @@ const { EJSON } = require("bson")
  * @throws {AbdbError}
  */
 async function apiPost(abdb, apiPath, params = {}, options = {}, tenantId = undefined) {
-  const endpoint = apiPath.substring(apiPath.lastIndexOf('/') + 1)
-  let res
-  try {
-    /** @type {Object}
-     * @mixes AxiosRequestConfig */
-    const reqConfig = {
-      headers: { [RUNTIME_HEADER]: abdb.runtimeNamespace },
-      auth: { // TODO: Implement basic auth, see CEXT-4617
-        username: 'user',
-        password: 'pass'
-      }
-    }
-    if (tenantId || abdb.tenantId) {
-      reqConfig.headers[TENANT_HEADER] = tenantId || abdb.tenantId
-    }
-    const body = params
-    if (options) {
-      body.options = options
-    }
-    res = await abdb.axiosClient.post(
-      `${ENDPOINT_URL}/v1/${apiPath}`,
-      EJSON.stringify(body, { relaxed: false }),
-      reqConfig
-    )
+  const body = params || {}
+  if (options) {
+    body.options = options
   }
-  catch (err) {
-    if (err.response?.data) {
-      throw new AbdbError(`${endpoint} failed: ${err.response.data.message}`, { cause: err })
-    }
-    throw err
-  }
-
-  if (!res.data.success) {
-    throw new AbdbError(`${endpoint} failed: ${res.data.message}`)
-  }
-  return res.data.data
+  return await apiRequest(abdb, `v1/${apiPath}`, 'POST', body, tenantId)
 }
 
 /**
- * Hit the abdb proxy service API and return the data field from the result
+ * Execute a GET request to the abdb proxy service API and return the data field from the result
  *
  * @param {Abdb} abdb
  * @param {string} apiPath abdb api url past <ENDPOINT>/v1/
@@ -62,7 +31,22 @@ async function apiPost(abdb, apiPath, params = {}, options = {}, tenantId = unde
  * @throws {AbdbError}
  */
 async function apiGet(abdb, apiPath, tenantId = undefined) {
-  const endpoint = apiPath.substring(apiPath.lastIndexOf('/') + 1)
+  return await apiRequest(abdb, `v1/${apiPath}`, 'GET', null, tenantId)
+}
+
+/**
+ * Internal helper method to construct and execute a request to the abdb proxy service API
+ *
+ * @param {Abdb} abdb
+ * @param {string} apiPath
+ * @param {string} method
+ * @param {Object=} body
+ * @param {string=} tenantId TODO: Remove tenancy in favor of runtime, see CEXT-4671
+ * @returns {Promise<*>}
+ * @throws {AbdbError}
+ */
+async function apiRequest(abdb, apiPath, method, body = {}, tenantId = undefined) {
+  const fullUrl = `${ENDPOINT_URL}/${apiPath}`
   let res
   try {
     /** @type {Object}
@@ -77,17 +61,28 @@ async function apiGet(abdb, apiPath, tenantId = undefined) {
     if (tenantId || abdb.tenantId) {
       reqConfig.headers[TENANT_HEADER] = tenantId || abdb.tenantId
     }
-    res = await abdb.axiosClient.get(`${ENDPOINT_URL}/v1/${apiPath}`, reqConfig)
+    if (method === 'GET') {
+      res = await abdb.axiosClient.get(fullUrl, reqConfig)
+    }
+    else {
+      res = await abdb.axiosClient.post(fullUrl, EJSON.stringify(body, { relaxed: false }), reqConfig)
+    }
   }
   catch (err) {
     if (err.response?.data) {
-      throw new AbdbError(`${endpoint} failed: ${err.response.data.message}`, { cause: err })
+      const reqId = err.response.data.requestId || err.response.headers[REQUEST_ID_HEADER]
+      throw new AbdbError(
+        `Request ${reqId} to ${apiPath} failed with code ${err.response.status}: ${err.response.data.message}`,
+        reqId,
+        { cause: err }
+      )
     }
     throw err
   }
 
   if (!res.data.success) {
-    throw new AbdbError(`${endpoint} failed: ${res.data.message}`)
+    const reqId = res.data.requestId || res.headers[REQUEST_ID_HEADER]
+    throw new AbdbError(`Request ${reqId} to ${apiPath} failed: ${res.data.message}`, reqId)
   }
   return res.data.data
 }
